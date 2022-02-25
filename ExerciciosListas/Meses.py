5c2f4e2286bbb1c82c72f95229a2d4f3ea73eeb1
Rudie23 <ruandiego100@gmail.com>
2022-02-25 20:58:08 -0300

    Programa que lê vogais e consoantes

---
+++ ExerciciosListas/Meses.py
@@ -0,0 +1,8 @@
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro'
+, 'Outubro', 'Novembro', 'Dezembro']
+n = 1
+while(n < 4):
+    mes = int(input("Escolha um mês (1-12): "))
+    if 1 <= mes < 13:
+        print(f'O mês é {meses[mes-1]}')
+    n += 1
