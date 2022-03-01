6201963bc51704e6f762a4bc68a33bd0608b2860
Rudie23 <ruandiego100@gmail.com>
2022-03-01 17:18:33 -0300

    Feito!

--- EstruturaDeRepetição/Aprendendo fatorial.py
+++ EstruturaDeRepetição/Aprendendo fatorial.py
@@ -3,8 +3,8 @@ c = n
 f = 1
 
 while c > 0:
-    print('{}'.format(c), end='')
+    print(f'{c}', end='')
     print(' x ' if c > 1 else ' = ', end='')
     f = f * c
     c = c - 1
-print('{}'.format(f))
+print(f'{f}')
