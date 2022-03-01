b3f6f1517fad3ac4cfc91d4782e892528acdebb7
Rudie23 <ruandiego100@gmail.com>
2022-03-01 17:13:36 -0300

    Feito!

--- EstruturaDeRepetição/Aprendendo fatorial.py
+++ EstruturaDeRepetição/Aprendendo fatorial.py
@@ -1,14 +1,3 @@
-
-numero = int(input('Digite um número: '))
-fatorial = 1
-
-while numero > 0:
-    fatorial = fatorial * numero
-    numero -= 1
-    print(fatorial)
-
-print(f'O fatorial do numero  {fatorial}.')
-
 n = int(input('Informe um número para obter o seu fatorial: '))
 c = n
 f = 1
@@ -17,5 +6,5 @@ while c > 0:
     print('{}'.format(c), end='')
     print(' x ' if c > 1 else ' = ', end='')
     f = f * c
-    c = c -1
+    c = c - 1
 print('{}'.format(f))
