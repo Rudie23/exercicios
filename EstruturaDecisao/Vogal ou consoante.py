2ce2730787392ab3b1d3d90929a8c1a49a14906f
Rudie23 <ruandiego100@gmail.com>
2022-03-04 23:23:37 -0300

    Feito!

--- EstruturaDecisao/Vogal ou consoante.py
+++ EstruturaDecisao/Vogal ou consoante.py
@@ -1,6 +1,7 @@
 letras = ['a', 'e', 'i', 'o', 'u']
 letra = input('Digite uma letra: ')
 
-for letra in letras:
-    if letra in letras:
-        print(f'É vogal')
+if letra in letras:
+    print(f'É vogal a letra "{letra}"')
+else:
+    print(f'É consoante a letra "{letra}"')
--- exercicios/Rever letras.py
+++ exercicios/Rever letras.py
@@ -1,10 +1,19 @@
+"""
+Nome na vertical em escada. Modifique o programa anterior de forma a mostrar o nome em formato de escada.
+
+F
+FU
+FUL
+FULA
+FULAN
+FULANO
+"""
 
 s = 'FULANO'
-
+new = ""
 for v in s:
-    v = v[:1]
-    print(v)
 
+    print(v)
 
 
 
