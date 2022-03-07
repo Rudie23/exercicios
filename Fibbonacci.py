d6e3e1da8647a2f0675c2e600d1417a5a9a1cb65
Rudie23 <ruandiego100@gmail.com>
2022-03-07 12:02:47 -0300

    Fibonacci in function!

--- Fibbonacci.py
+++ Fibbonacci.py
@@ -3,3 +3,15 @@ a, b = 0, 1
 while a < 10000000:
     print(a, end=' ')
     a, b = b, a+b
+print('\n')
+def fib(n):
+    '''Return a list containing the Fibonacci series up to on'''
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+        print(a, end=' ')
+    return result
+
+fib(100)
