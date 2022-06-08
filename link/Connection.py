6c722d166a5f5da15fc8e348e8e2d2ed95f5d416
Ruan Diego <ruandiego100@gmail.com>
2022-06-08 17:23:52 -0300

    Connection with internet through JSON

---
+++ link/Connection.py
@@ -0,0 +1,11 @@
+import  urllib.request
+
+
+def ConectInternet():
+    objurl = urllib.request.urlopen("https://www.google.com")
+
+    if objurl.getcode() == 200:
+        data = objurl.read()
+        print(data)
+
+ConectInternet()
